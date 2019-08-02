d52f5a623ebd82f7bd3c7cf2bee042363385878e
Alon Robinson <alon.robinson@ge.com>
2019-08-02 15:56:39 -0500

    update tests

--- test.js
+++ test.js
@@ -1,1 +1,32 @@
 const tap = require('tap');
+const bufferReplace = require('./index');
+
+tap.test('replaceFirst replaces the first occurrence of a character', test => {
+  const originalBuffer = Buffer.from('himynameis');
+  const expectedBuffer = Buffer.from('homynameis');
+  test.deepEqual(
+    bufferReplace.replaceFirst(originalBuffer, 'i', 'o'),
+    expectedBuffer,
+  );
+  test.end();
+});
+
+tap.test('replaceLast replaces the last occurrence of a character', test => {
+  const originalBuffer = Buffer.from('himynameis');
+  const expectedBuffer = Buffer.from('himynameos');
+  test.deepEqual(
+    bufferReplace.replaceLast(originalBuffer, 'i', 'o'),
+    expectedBuffer,
+  );
+  test.end();
+});
+
+tap.test('replaceAll replaces all occurrences of a character', test => {
+  const originalBuffer = Buffer.from('himynameis');
+  const expectedBuffer = Buffer.from('homynameos');
+  test.deepEqual(
+    bufferReplace.replaceAll(originalBuffer, 'i', 'o'),
+    expectedBuffer,
+  );
+  test.end();
+});
